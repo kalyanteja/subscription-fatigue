791f8690cea5f195dbfb024164a56ae3f5f5d811
kalyanteja <kalyansmacks@gmail.com>
2019-06-24 03:04:00 +0530

    added subscription & input types for graphql

--- app.js
+++ app.js
@@ -5,16 +5,35 @@ const { buildSchema } = require('graphql');
 
 const app = express();
 
+const subscriptions = [];
+
 app.use(bodyParser.json());
 
 app.use('/graphql', graphqlHttp({
     schema: buildSchema(`
+        type Subscription {
+            _id: ID!
+            name: String!
+            companyId: Int!
+            description: String
+            period: String!
+            date: String!
+        }
+
+        input SubscriptionInput {
+            name: String!
+            companyId: Int!
+            description: String
+            period: String!
+            date: String
+        }
+
         type RootQuery {
-            subscriptions: [String!]!
+            subscriptions: [Subscription!]!
         }
 
         type RootMutation {
-            addSubscription(name: String): String
+            addSubscription(subscriptionInput: SubscriptionInput): Subscription
         }
 
         schema {
@@ -24,11 +43,21 @@ app.use('/graphql', graphqlHttp({
     `),
     rootValue: {
         subscriptions: () => {
-            return ['Netflix', 'Spotify', 'HotStar'];
+            return subscriptions;
         },
         addSubscription: (args) => {
-            const addedSubscription = args.name;
-            return addedSubscription;
+            const subscription = {
+                _id: Math.random().toString(),
+                name: args.subscriptionInput.name,
+                companyId: args.subscriptionInput.companyId,
+                description: args.subscriptionInput.description,
+                period: args.subscriptionInput.period,
+                date: new Date().toISOString()
+            };
+
+            subscriptions.push(subscription);
+
+            return subscription;
         }
     },
     graphiql: true
